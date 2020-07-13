fd385ec281a4cee5af700618ee4d9f7017fb3bd9
Sarang Ravindra <sarang7822ravindra@gmail.com>
2020-07-13 19:21:43 +0530

    Linked list design

---
+++ LinkedLists/LinkedList.py
@@ -0,0 +1,97 @@
+class Node:
+    def __init__(self):
+        self.val = None
+        self.next = None
+
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.root = None
+        self.tail = None
+        self.n = 0
+
+    def printList(self, name):
+        t = self.root
+        print(name)
+        while(t != None):
+            print(t.val, end=" ")
+            t = t.next
+        print()
+
+    def get(self, index: int) -> int:
+        if index >= self.n:
+            return -1
+        else:
+            t = self.root
+            c = 0
+            while(c < index):
+                t = t.next
+                c += 1
+
+            return t.val
+
+    def addAtHead(self, val: int) -> None:
+        node = Node()
+        node.val = val
+        if(self.root == None):
+            self.tail = node
+        node.next = self.root
+        self.root = node
+        self.n += 1
+
+    def addAtTail(self, val: int) -> None:
+        node = Node()
+        node.val = val
+        node.next = None
+        if(self.root == None):
+            self.root = node
+            self.tail = node
+            self.n += 1
+            return
+        self.tail.next = node
+        self.tail = node
+        self.n += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if(index <= self.n):
+            if(index == self.n):
+                self.addAtTail(val)
+                return
+            if(index == 0):
+                self.addAtHead(val)
+                return
+            node = Node()
+            node.val = val
+            t = self.root
+            c = 0
+            while(c < index-1):
+                t = t.next
+                c += 1
+            node.next = t.next
+            t.next = node
+            self.n += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        p = None
+        if(index < self.n):
+            if(index == 0):
+                self.root = self.root.next
+                self.n -= 1
+                return
+            if(index == self.n-1):
+                p = self.root
+                while(p.next != self.tail):
+                    p = p.next
+                self.tail = p
+                self.tail.next = None
+                self.n -= 1
+                return
+            t = self.root
+            c = 0
+            while(c < index):
+                p = t
+                t = t.next
+                c += 1
+            p.next = t.next
+            self.n -= 1
