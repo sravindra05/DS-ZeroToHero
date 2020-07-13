e2c6c9599068c5988ad84c8cce0a93bd53768ecd
Sarang Ravindra <sarang7822ravindra@gmail.com>
2020-07-13 19:31:54 +0530

    Linked list design

--- LinkedLists/LinkedList.py
+++ LinkedLists/LinkedList.py
@@ -1,18 +1,33 @@
 class Node:
+    """
+    Class definition for each node in the linked list
+    val  : Data held in node
+    next : Pointer to next node 
+    """
+
     def __init__(self):
         self.val = None
         self.next = None
 
 
 class MyLinkedList:
+    """
+    Class defnition of the actual Linked List
+    head : Head of the list
+    tail : Tail of the list
+    n    : Number of nodes in the list
+    """
 
     def __init__(self):
-        self.root = None
+        self.head = None
         self.tail = None
         self.n = 0
 
     def printList(self, name):
-        t = self.root
+        """
+        Prints the values held in the linked list in order
+        """
+        t = self.head
         print(name)
         while(t != None):
             print(t.val, end=" ")
@@ -20,10 +35,14 @@ class MyLinkedList:
         print()
 
     def get(self, index: int) -> int:
+        """
+        Gets an element at a particular index
+        returns -1 incase of invalid index 
+        """
         if index >= self.n:
             return -1
         else:
-            t = self.root
+            t = self.head
             c = 0
             while(c < index):
                 t = t.next
@@ -32,20 +51,26 @@ class MyLinkedList:
             return t.val
 
     def addAtHead(self, val: int) -> None:
+        """
+        Add an element to the head of the list
+        """
         node = Node()
         node.val = val
-        if(self.root == None):
+        if(self.head == None):
             self.tail = node
-        node.next = self.root
-        self.root = node
+        node.next = self.head
+        self.head = node
         self.n += 1
 
     def addAtTail(self, val: int) -> None:
+        """
+        Adds an element to the tail of the list
+        """
         node = Node()
         node.val = val
         node.next = None
-        if(self.root == None):
-            self.root = node
+        if(self.head == None):
+            self.head = node
             self.tail = node
             self.n += 1
             return
@@ -54,6 +79,10 @@ class MyLinkedList:
         self.n += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Adds an element at a particular index
+        Does nothing incase of an invalid index 
+        """
         if(index <= self.n):
             if(index == self.n):
                 self.addAtTail(val)
@@ -63,7 +92,7 @@ class MyLinkedList:
                 return
             node = Node()
             node.val = val
-            t = self.root
+            t = self.head
             c = 0
             while(c < index-1):
                 t = t.next
@@ -73,21 +102,25 @@ class MyLinkedList:
             self.n += 1
 
     def deleteAtIndex(self, index: int) -> None:
+        """
+        Deletes an element at a particular index
+        Does nothing incase of an invalid index 
+        """
         p = None
         if(index < self.n):
             if(index == 0):
-                self.root = self.root.next
+                self.head = self.head.next
                 self.n -= 1
                 return
             if(index == self.n-1):
-                p = self.root
+                p = self.head
                 while(p.next != self.tail):
                     p = p.next
                 self.tail = p
                 self.tail.next = None
                 self.n -= 1
                 return
-            t = self.root
+            t = self.head
             c = 0
             while(c < index):
                 p = t
